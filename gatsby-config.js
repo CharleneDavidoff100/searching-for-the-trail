6d548494e1cc94520089de48273351dac8797597
Charlene Davidoff <55562227+CharleneDavidoff100@users.noreply.github.com>
2020-03-26 19:37:00 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,9 +18,9 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Contentful starter',
+    title: 'Charlene Davidoff :: Product Manager',
   },
-  pathPrefix: '/gatsby-contentful-starter',
+  pathPrefix: '/',
   plugins: [
     'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
