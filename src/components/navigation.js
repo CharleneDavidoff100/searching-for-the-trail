05ca5605927fe512eb34592c6fc61f6aab95f958
Charlene Davidoff <55562227+CharleneDavidoff100@users.noreply.github.com>
2020-03-30 13:59:55 -0700

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,15 +1,16 @@
-iimport React from 'react'
+import React from 'react'
 import { Link } from 'gatsby'
 import styles from './navigation.module.css'
-
 
 export default () => (
   <nav role="navigation">
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
-        <Link to="/">Searching for the Trail</Link>
+        <Link to="/">Searching For The Trail</Link>
       </li>
-     </ul>
+      <li className={styles.navigationItem}>
+        <Link to="/blog/"></Link>
+      </li>
+    </ul>
   </nav>
 )
-
