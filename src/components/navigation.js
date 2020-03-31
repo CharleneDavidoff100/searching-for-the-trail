058d3c7c76b3ab49af27f6184fff394e678691f9
Charlene Davidoff <55562227+CharleneDavidoff100@users.noreply.github.com>
2020-03-30 17:13:44 -0700

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,7 +6,7 @@ export default () => (
   <nav role="navigation">
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
-        <Link to="/">The Trail</Link>
+        <Link to="/">SEARCHING FOR THE TRAIL</Link>
       </li>
       <li className={styles.navigationItem}>
         <Link to="/blog/"> </Link>
